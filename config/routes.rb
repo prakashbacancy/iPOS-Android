363029acc3c770bbc4dd941418256c658a49ee66
urmit-prajapati <urmit.prajapati@bacancy.com>
2021-07-01 15:32:54 +0530

    routes for user and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { passwords: 'passwords', sessions: 'sessions' }
+
+  root to: 'homes#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :users
+  end
 end
