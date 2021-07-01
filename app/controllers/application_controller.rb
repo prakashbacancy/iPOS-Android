e2112aaaa176c95bf4c4e1102af0f99bb66ae614
prakashbacancy <prakash.chaudhary@bacancy.com>
2021-07-01 14:43:27 +0530

    after login redirect on subdomain url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+  before_action :current_location
+
+	def current_location
+    @current_location ||= if user_signed_in?
+      current_user.location || Location.find_by(subdomain: request.subdomain)
+    else
+      Location.find_by(subdomain: request.subdomain)
+    end
+  end
+
+  # Redirects to a path after sign in.
+  def after_sign_in_path_for(resource)
+    if user_signed_in?
+      root_url(subdomain: resource&.location&.subdomain.to_s)
+    elsif current_user.role.try(:name) == 'super_admin'
+      signin_with_otp_already? ? admin_users_path : admin_send_otp_path
+    end
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    root_url(subdomain: nil)
+  end
 end
